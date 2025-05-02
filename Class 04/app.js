dcaca1aad585405df30b0e583eb74baa95bac3f4
Meer Quais <meerquais017@gmail.com>
2025-05-02 21:51:19 +0500

    Functions

---
+++ Class 04/app.js
@@ -0,0 +1,175 @@
+console.log("Js Connected!");
+
+// function fuctionName(){
+//     // code
+// }
+
+
+
+// greet()
+// greet()
+// greet()
+// greet()
+// function showLine(){
+//     console.log("----------------------");
+    
+// }
+// showLine()
+
+
+// function greet(){
+//     console.log("Hello Student!");
+// }
+// greet()
+
+// showLine()
+
+// function sayWelcome(){
+//     console.log("Welcome to Functions!");
+// }
+
+// sayWelcome()
+// showLine()
+
+// function greetUser(name){
+//     console.log("Hello " + name);
+// }
+
+// greetUser("Meer");
+// greetUser("Ali")
+
+
+// let a = 40;
+// let b = 340;
+
+// let result = a + b;
+// console.log(result)
+
+// function add(a , b){
+//     console.log(a + b);
+// }
+
+// add(4,10)
+// add(9,20)
+
+// function aor(length , width){
+//     console.log("Area is : " , length * width);
+// }
+
+
+// aor(2,3)
+
+
+// let value1 = +prompt("Type value 1");
+// let value2 = +prompt("Type value 2");
+
+// function sub(a , b){
+//     console.log(a - b );
+// }
+
+// sub(4,3)
+// sub(10,20);
+// sub(value1,value2);
+
+// function multi(a , b ){
+//     return a * b;
+// }
+
+// let result = multi(9,7);
+
+// console.log("Results is: " + result );
+
+// function getGreeting(name){
+//     return "Hello " + name + "!";
+// }
+
+// let greetMsg = getGreeting("Meer");
+
+// console.log(greetMsg);
+
+
+// function isEven(num){
+//     return num % 2 === 0;
+// }
+
+// console.log(isEven(7));
+// console.log(isEven(6));
+
+
+// Function Expression 
+
+// let sayHi = function(){
+//     console.log("Hi There!");
+// }
+
+// sayHi()
+
+
+// Arrow Functions // ES6
+ 
+// const function = (param)=>{
+//     // code 
+// }
+// Function Expression 
+// const greet = ()=>{
+//     console.log("Arrow Function says Hi!");
+// }
+
+// greet();
+
+
+// const add = (a = 1, b = 1 )=>{
+//     console.log(a + b);
+// }
+
+// add(10)
+
+// const greet = (name = "Guest!")=>{
+//     console.log("Hello, " + name
+//     );
+// }
+
+// greet()
+// NESTED 
+// function outer(){
+//     console.log("Outer Function!");
+
+//     function inner(){
+//         console.log("Inner Function");
+//     }
+//     inner()
+    
+// }
+
+// outer()
+
+// function fullName(firstName , lastName){
+
+//     function combine(){
+//         return firstName + " " + lastName;
+//     }
+
+//    return combine();
+// }
+
+// console.log(fullName("Meer","Quais"))
+
+// IIFE = Immediately Invoked Function Expression!
+
+// (function(){
+//     console.log("This runs Immediately");
+// })();
+
+// (function(){
+//     console.log("This is IIFE");
+// })();
+
+
+// (function(name){
+//     console.log("Welcome, " + name);
+    
+// })("Meer")
+
+
+
+
